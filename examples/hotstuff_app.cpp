cf510bac2cd28a71c83f2dc43b85706b20875d49
martina97 <martinadem97@gmail.com>
2024-01-15 15:45:00 +0100

    cancellati file hotstuff.conf, hotstuff-sec0.conf, hotstuff-sec1.conf, hotstuff-sec2.conf, hotstuff-sec3.conf, log0, log1, log2, log3

--- examples/hotstuff_app.cpp
+++ examples/hotstuff_app.cpp
@@ -147,6 +147,61 @@ salticidae::BoxObj<HotStuffApp> papp = nullptr;
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
+    Config config("hotstuff.conf");     // classe che gestisce le opzioni di configurazione per un'applicazione
+    std::cout << "---- DOPO CONFIG ---- " << std::endl;
+    ElapsedTime elapsed;
+    elapsed.start();
+    std::cout << "---- DOPO elapsed.start() ---- " << std::endl;
+    std::cout << "Elapsed time: " << elapsed.elapsed_sec << " seconds\n";
+    std::cout << "CPU time: " << elapsed.cpu_elapsed_sec << " seconds\n";
+
+    auto opt_blk_size = Config::OptValInt::create(1);
+    auto opt_parent_limit = Config::OptValInt::create(-1);
+    auto opt_stat_period = Config::OptValDouble::create(10);
+    auto opt_replicas = Config::OptValStrVec::create();
+    auto opt_idx = Config::OptValInt::create(0);
+    auto opt_client_port = Config::OptValInt::create(-1);
+    auto opt_privkey = Config::OptValStr::create();
+    auto opt_tls_privkey = Config::OptValStr::create();
+    auto opt_tls_cert = Config::OptValStr::create();
+    auto opt_help = Config::OptValFlag::create(false);
+    auto opt_pace_maker = Config::OptValStr::create("dummy");
+    auto opt_fixed_proposer = Config::OptValInt::create(1);
+    auto opt_base_timeout = Config::OptValDouble::create(1);
+    auto opt_prop_delay = Config::OptValDouble::create(1);
+    auto opt_imp_timeout = Config::OptValDouble::create(11);
+    auto opt_nworker = Config::OptValInt::create(1);
+    auto opt_repnworker = Config::OptValInt::create(1);
+    auto opt_repburst = Config::OptValInt::create(100);
+    auto opt_clinworker = Config::OptValInt::create(8);
+    auto opt_cliburst = Config::OptValInt::create(1000);
+    auto opt_notls = Config::OptValFlag::create(false);
+    auto opt_max_rep_msg = Config::OptValInt::create(4 << 20); // 4M by default
+    auto opt_max_cli_msg = Config::OptValInt::create(65536); // 64K by default
+
+    config.add_opt("block-size", opt_blk_size, Config::SET_VAL);
+    config.add_opt("parent-limit", opt_parent_limit, Config::SET_VAL);
+    config.add_opt("stat-period", opt_stat_period, Config::SET_VAL);
+    config.add_opt("replica", opt_replicas, Config::APPEND, 'a', "add an replica to the list");
+    config.add_opt("idx", opt_idx, Config::SET_VAL, 'i', "specify the index in the replica list");
+    config.add_opt("cport", opt_client_port, Config::SET_VAL, 'c', "specify the port listening for clients");
+    config.add_opt("privkey", opt_privkey, Config::SET_VAL);
+    config.add_opt("tls-privkey", opt_tls_privkey, Config::SET_VAL);
+    config.add_opt("tls-cert", opt_tls_cert, Config::SET_VAL);
+    config.add_opt("pace-maker", opt_pace_maker, Config::SET_VAL, 'p', "specify pace maker (dummy, rr)");
+    config.add_opt("proposer", opt_fixed_proposer, Config::SET_VAL, 'l', "set the fixed proposer (for dummy)");
+    config.add_opt("base-timeout", opt_base_timeout, Config::SET_VAL, 't', "set the initial timeout for the Round-Robin Pacemaker");
+    config.add_opt("prop-delay", opt_prop_delay, Config::SET_VAL, 't', "set the delay that follows the timeout for the Round-Robin Pacemaker");
+    config.add_opt("imp-timeout", opt_imp_timeout, Config::SET_VAL, 'u', "set impeachment timeout (for sticky)");
+    config.add_opt("nworker", opt_nworker, Config::SET_VAL, 'n', "the number of threads for verification");
+    config.add_opt("repnworker", opt_repnworker, Config::SET_VAL, 'm', "the number of threads for replica network");
+    config.add_opt("repburst", opt_repburst, Config::SET_VAL, 'b', "");
+    config.add_opt("clinworker", opt_clinworker, Config::SET_VAL, 'M', "the number of threads for client network");
+    config.add_opt("cliburst", opt_cliburst, Config::SET_VAL, 'B', "");
+    config.add_opt("notls", opt_notls, Config::SWITCH_ON, 's', "disable TLS");
+    config.add_opt("max-rep-msg", opt_max_rep_msg, Config::SET_VAL, 'S', "the maximum replica message size");
+    config.add_opt("max-cli-msg", opt_max_cli_msg, Config::SET_VAL, 'S', "the maximum client message size");
+    config.add_opt("help", opt_help, Config::SWITCH_ON, 'h', "show this help info");
 }
 
 
